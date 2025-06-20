167e3cca6af257387b64ddb7e75fdb30863d9b3d
Elvis Serrano <serranoelvis38@gmail.com>
2025-06-20 08:37:45 -0500

    Adición de redimensionamiento del viewport

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import climaActual from './current-weather.js'
+import { viewportSize } from './utils/viewport.js';
+
+
+const $app = document.querySelector('#app')
+const $loader = document.querySelector('#loading')
+viewportSize($app)
+viewportSize($loader)
 
 climaActual();
